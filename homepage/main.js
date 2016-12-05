6f29f58693000fc62ccf1fa3ec9448415b8ab6a1
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-12-05 11:16:59 -0700

    init(commit) Initial commit
    Added folder for homepage
    Added html file
    Added css folder and file
    Added js file
    Created folders for three canyons

---
+++ homepage/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// If these three work, continue
+// console.log("Hello, world!");
+// console.log(document.title);
+// document.write("My wife is Jessica");
+
+//  Continue with JS
+// alert("Disclaimer: \n1.\tI do not endorse any products on these pages. \n2.\tAll trails are out-and-back. \n3. \n4.");
