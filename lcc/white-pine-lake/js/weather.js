4962ffccef31a6a03d1b09837bbceb8bf14be7cf
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-12-05 14:15:26 -0700

    "feat(core) Complete weather with white pine lake html and js"

--- lcc/white-pine-lake/js/weather.js
+++ lcc/white-pine-lake/js/weather.js
@@ -18,18 +18,38 @@ $(document).ready(function(){
 $.getJSON(api, function(data) {
   // console.log(data.coord.lon);
   // alert(data.coord.lon);
-  var temp=data.main.temp.toFixed(1);
-  console.log("The current temperature is: ", temp + "°F");
-  $("#temp").html(temp + "°F");
-  console.log("The current atmospheric pressure is:", data.main.pressure + "hpa");
-  console.log("The current humidity is:", data.main.humidity + "%");
-  console.log("The low temperature today is:", data.main.temp_min.toFixed(0) + "°F");
-  console.log("The high temperature today is:", data.main.temp_max.toFixed(0) + "°F");
+  //declare the variables
+  var temp = data.main.temp.toFixed(1);
+  var temp_min = data.main.temp_min.toFixed(0);
+  var temp_max = data.main.temp_max.toFixed(0);
+  var weatherType = data.weather[0].description;
+  var windSpeed=data.wind.speed;
+  var windDirection=data.wind.deg;
+  var humidity = data.main.humidity;
+  var pressure = data.main.pressure;
+
+  //log to see if it works, then populate page
+  // console.log("The current temperature is: ", temp + "°F");
+  $("#temp").html("The current temperature is: " + temp + "°F<br>");
+
+  // console.log("The low temperature today is:", data.main.temp_min.toFixed(0) + "°F");
+  $("#temp_min").html("The low for today is expected to be " + temp_min + "°F");
+
+  // console.log("The high temperature today is:", data.main.temp_max.toFixed(0) + "°F");
+  $("#temp_max").html("The high for today is expected to be " + temp_max + "°F");
+
+  // console.log("The current weather is: " + weatherType);
+  $("#weatherType").html("It is currently " + weatherType);
+
+  // console.log("The current wind speed is: ", data.wind.speed + "mph");
+  // console.log("The current wind direction is:", data.wind.deg + "°");
+  $("#windSpeed").html("The wind is currently blowing " + windSpeed + "mph with a heading of " + windDirection + "°.");
+
+  // console.log("The current atmospheric pressure is:", data.main.pressure + "hpa");
+  // console.log("The current humidity is:", data.main.humidity + "%");
+  $("#humidity").html("The humidity is " + humidity + "%<br>" + "with atmospheric pressure of " + pressure + "hpa")
   // console.log("The country this is in is: ", data.sys.country);
-  console.log("The current weather is: ", data.weather[0].description);
   // console.log("The current weather description is: ", data.weather[0].main);
-  console.log("The current wind speed is:", data.wind.speed+ "mph");
-  console.log("The current wind direction is:", data.wind.deg +"°");
   // alert(data.main.temp_min);
   // document.write(data.main.temp_min);
   // alert("The current temperature is: ", data.main.temp)
